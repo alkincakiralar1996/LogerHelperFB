d2e918b688faf5402046be2f06f9766845f1982e
Alkın Çakıralar <a.cakiralar@gmail.com>
2022-01-02 22:35:10 +0300

    Release 1.0.0

--- Example/LogerHelperFB/ViewController.swift
+++ Example/LogerHelperFB/ViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import LogerHelperFB
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        FBLoggerHelper.init().priTest()
     }
 
     override func didReceiveMemoryWarning() {
