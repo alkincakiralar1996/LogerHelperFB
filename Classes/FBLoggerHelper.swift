1b64bedc0df61459db51f9eb3951bfe03b1fbed7
Alkın Çakıralar <a.cakiralar@gmail.com>
2022-01-02 22:23:34 +0300

    Initial Commit

---
+++ Classes/FBLoggerHelper.swift
@@ -0,0 +1,18 @@
+//
+//  FBLoggerHelper.swift
+//  LogerHelperFB
+//
+//  Created by Alkın Çakıralar on 2.01.2022.
+//
+
+import Foundation
+
+public class FBLoggerHelper {
+    
+    public init() {}
+    
+    public func priTest() {
+        print("test")
+    }
+    
+}
---
+++ Example/Pods/Target Support Files/LogerHelperFB/LogerHelperFB-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LogerHelperFB : NSObject
+@end
+@implementation PodsDummy_LogerHelperFB
+@end
